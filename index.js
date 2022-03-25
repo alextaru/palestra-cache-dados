958e3a437ee2b14cad7e77d8c1e00890a0fc52e3
alexandre goncalves <alex.taru@yahoo.com.br>
2022-03-25 18:05:01 -0300

    implementando redis

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const produtoModel = require("./models/produtos");
+const { createClient } = require("redis");
+const client = createClient();
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 (async () => {
-    const data = await buscarProdutos();
-    console.log(data);
+    await client.connect();
+    console.time("tempo");
+    for (let index = 0; index < 1000; index++) {
+      await buscarProdutos();
+      //await buscarProdutosRedis();
+    }
+    console.timeEnd("tempo");
+    //console.log(data);
     process.exit();
 })();
 
 async function buscarProdutos() {
   const data = await produtoModel.findOne();
   return data.dataValues;
+}
+
+async function buscarProdutosRedis() {
+  let produto = await client.get('produto');
+  if (!produto) {
+    const data = await produtoModel.findOne();
+    produto = JSON.stringify(data);
+    await client.set('produto', produto,{EX: 20});
+  }
+  return JSON.parse(produto);
 }
